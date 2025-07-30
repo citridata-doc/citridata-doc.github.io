4886af8b2442dafed4dcae69c39f98c8619eed70
Jose Checa <citridata.doc@gmail.com>
2025-07-30 12:27:09 +0200

    Init README.md

--- .docusaurus/client-modules.js
+++
@@ -1,7 +0,0 @@
-export default [
-  require("/Users/jcheca/vsPython/citridata-docusaurus/.docusaurus/docusaurus-plugin-css-cascade-layers/default/layers.css"),
-  require("/Users/jcheca/vsPython/citridata-docusaurus/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/jcheca/vsPython/citridata-docusaurus/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/jcheca/vsPython/citridata-docusaurus/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/Users/jcheca/vsPython/citridata-docusaurus/src/css/custom.css"),
-];
